50bcd2b14f5e913ffedb47987244e51927ef5692
aklsh <akileshkannan@gmail.com>
2019-06-09 16:52:26 +0530

    added header comment

--- objectTracker.py
+++ objectTracker.py
@@ -1,14 +1,29 @@
+########################################################################
+# Done By: Akilesh Kannan
+# Version: v1.1
+# Last Updated: Jun 04, 2019
+########################################################################
+
 import numpy as np
 import cv2
+import sys
 
 cam = cv2.VideoCapture(0)
 kernel = np.ones((5,5))
+color=str(sys.argv[1])
 
 while(True):
     
     ret, frame = cam.read()
 
     hsvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_H = {'red':0, 'green':29}
+    lower_S = {'red':230, 'green':86}
+    lower_V = {'red':230, 'green':6}
+    upper_H = {'red':160, 'green':100}
+    upper_S = {'red':255, 'green':255}
+    upper_V = {'red':255, 'green':255}
 
     lower = np.array([29, 86, 6])
     upper = np.array([100, 255, 255])
